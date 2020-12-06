69be005bcb316a074c7c12a2dc76f47201463ae3
Max Katsev <mkatsev@fb.com>
2020-12-06 23:35:46 +0000

    tab width

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -2,29 +2,29 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), std::io::Error> {
-    let values: Vec<_> = BufReader::new(File::open("input/1")?)
-        .lines()
-        .map(|line| line.unwrap().parse::<i64>().unwrap())
-        .collect();
+  let values: Vec<_> = BufReader::new(File::open("input/1")?)
+    .lines()
+    .map(|line| line.unwrap().parse::<i64>().unwrap())
+    .collect();
 
-    'outer: for i in values.iter() {
-        for j in values.iter() {
-            if i + j == 2020 {
-                println!("{}", i * j);
-                break 'outer;
-            }
-        }
+  'outer: for i in values.iter() {
+    for j in values.iter() {
+      if i + j == 2020 {
+        println!("{}", i * j);
+        break 'outer;
+      }
     }
+  }
 
-    'outer2: for i in values.iter() {
-        for j in values.iter() {
-            for k in values.iter() {
-                if i + j + k == 2020 {
-                    println!("{}", i * j * k);
-                    break 'outer2;
-                }
-            }
+  'outer2: for i in values.iter() {
+    for j in values.iter() {
+      for k in values.iter() {
+        if i + j + k == 2020 {
+          println!("{}", i * j * k);
+          break 'outer2;
         }
+      }
     }
-    Ok(())
+  }
+  Ok(())
 }
--- src/bin/3.rs
+++ src/bin/3.rs
@@ -2,35 +2,35 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn count(input: &Vec<String>, right: usize, down: usize) -> i64 {
-    let mut i = 0;
-    let mut j = 0;
-    let mut count = 0;
-    while i < input.len() {
-        let line = input[i].as_bytes();
-        if line[j] == b'#' {
-            count += 1;
-        }
-        j += right;
-        if j >= line.len() {
-            j -= line.len();
-        }
-        i += down;
+  let mut i = 0;
+  let mut j = 0;
+  let mut count = 0;
+  while i < input.len() {
+    let line = input[i].as_bytes();
+    if line[j] == b'#' {
+      count += 1;
     }
-    count
+    j += right;
+    if j >= line.len() {
+      j -= line.len();
+    }
+    i += down;
+  }
+  count
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let input: Vec<_> = BufReader::new(File::open("input/3")?)
-        .lines()
-        .collect::<Result<_, _>>()?;
-    println!(
-        "{} {}",
-        count(&input, 3, 1),
-        count(&input, 1, 1)
-            * count(&input, 3, 1)
-            * count(&input, 5, 1)
-            * count(&input, 7, 1)
-            * count(&input, 1, 2)
-    );
-    Ok(())
+  let input: Vec<_> = BufReader::new(File::open("input/3")?)
+    .lines()
+    .collect::<Result<_, _>>()?;
+  println!(
+    "{} {}",
+    count(&input, 3, 1),
+    count(&input, 1, 1)
+      * count(&input, 3, 1)
+      * count(&input, 5, 1)
+      * count(&input, 7, 1)
+      * count(&input, 1, 2)
+  );
+  Ok(())
 }
